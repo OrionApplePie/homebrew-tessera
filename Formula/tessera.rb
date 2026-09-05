b2d645b1829e866ed85049caa58635f1f81f556f
Alexander Ten <orionapplepie@gmail.com>
2026-09-05 15:58:12 +1000

    tessera 0.1.2

--- Formula/tessera.rb
+++ Formula/tessera.rb
@@ -1,8 +1,8 @@
 class Tessera < Formula
   desc "Native macOS window switcher with a map of every Space"
   homepage "https://github.com/OrionApplePie/Tessera"
-  url "https://github.com/OrionApplePie/Tessera/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "2ea7129ff64c3acbd55df57011d9d9b527d1773ff537596e95e0d35483c74fb0"
+  url "https://github.com/OrionApplePie/Tessera/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "f71426f3549535e816b4d86fd1c164f176fa8e8a1fe1920d3a18acda76c72283"
   license "GPL-3.0-or-later"
   head "https://github.com/OrionApplePie/Tessera.git", branch: "main"
 
